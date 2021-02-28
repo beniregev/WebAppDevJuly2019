1e0d76ffb3dbc02f274e70369abc259039c8f829
Binyamin Regev <beniregev@gmail.com>
2021-02-28 22:06:41 +0200

    Add meetings #16 files

---
+++ 2019-08-26 Meeting 16 Variables Declaration/exercise-1/script.js
@@ -0,0 +1,7 @@
+$(function () {
+    console.log("Java Script Variables: Primitives");
+    var x = 3;
+    console.log("variable x is a " + typeof (x) + " and have value: " + x);
+    x = "Benny Regev";
+    console.log("variable x is a " + typeof (x) + " and have value: " + x);
+});
